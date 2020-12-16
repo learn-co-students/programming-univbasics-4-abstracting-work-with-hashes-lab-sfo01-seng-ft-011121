cac50a908498ad899d65dce65a2b491319346bb7
Erik Huang <erikhuangdrums@gmail.com>
2020-12-16 23:34:20 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,12 +1,12 @@
 def my_hash_creator(k, v)
-  return { k => v }
+  { k => v }
 end
 
 def read_from_hash(hash, k)
-  hash[k] ? hash[k] : nil
+  hash[k]
 end
 
 def update_counting_hash(hash, k)
-  hash.has_key?(k) ? hash[k] += 1 : hash[k] = 1
-  return hash
+  hash[k] ? hash[k] += 1 : hash[k] = 1
+  hash
 end
